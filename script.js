7ef8c02dfcc7db7f1365d9144d4b660a844ead08
Brang Mai <brangmai100@gmail.com>
2021-06-14 22:29:18 -0400

    Added more functionalities

--- script.js
+++ script.js
@@ -2,13 +2,28 @@ let numRows = 0;
 let numCols = 0;
 let colorSelected; 
 
-var addR = document.querySelectorAll("button.addR()"),
-    addC = document.querySelectorAll("botton.addC"),
-    removeR = document.querySelectorAll("botton.removeC"),
-    fillU = document.querySelectorAll("botton.fillU()"),
-    fill = document.querySelectorAll("botton.fill()"),
-    clearAll = document.querySelectorAll("botton.clearAll()"), 
-    selected = document.callElementByTagName("select");
+var addRow = document.getElementsByTagName("botton")[0],
+    addCol = document.getElementsByTagName("botton")[1],
+    removeRow = document.getElementsByTagName("botton")[2],
+    removeCol = document.getElementsByTagName("botton")[3],
+    fillColorAll = document.getElementsByTagName("botton")[4],
+    fillColor = document.getElementsByTagName("botton")[5],
+    clearAllSquares = document.getElementsByTagName("botton")[6];
+
+    // addR = document.querySelectorAll("button.addR()"),
+    // addC = document.querySelectorAll("botton.addC"),
+    // removeR = document.querySelectorAll("botton.removeC"),
+    // fillU = document.querySelectorAll("botton.fillU()"),
+    // fill = document.querySelectorAll("botton.fill()"),
+    // clearAll = document.querySelectorAll("botton.clearAll()"), 
+addRow.addEventListener("click", addR);
+addCol.addEventListener("click", addC);
+removeRow.addEventListener("click", removeR);
+removeCol.addEventListener("click", removeC);
+fillColorAll.addEventListener("click", fillU);
+fillColor.addEventListener("click", fill);
+clearAllSquares.addEventListener("click", addR);
+addRow.addEventListener("click", clearAll);
 
 var squareBox = document.querySelector('td');
 
